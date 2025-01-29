faada0ee7fe0f240ae4439a023d167b6c5c767f0
new <sabbirsce72@gmail.com>
2025-01-30 00:17:16 +0600

    cypress 1st case

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+// test suite name
+describe("Tutorialspoint Test", function () {
+  // Test case
+  it("Scenario 1", function () {
+    // test step for URL launching
+    cy.visit("https://www.google.com/");
+  });
+});
