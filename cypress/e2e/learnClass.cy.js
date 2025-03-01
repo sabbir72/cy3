d8f472e2e412580410bc726f40671c5a961c6e36
new <sabbirsce72@gmail.com>
2025-03-01 15:48:49 +0600

    lastupdate

--- cypress/e2e/learnClass.cy.js
+++ cypress/e2e/learnClass.cy.js
@@ -1,20 +1,26 @@
 describe("My 1st Test", () => {
   it('finds the content "type"', () => {
-    
-
     cy.visit("https://example.cypress.io");
     cy.contains("get").click();
     cy.url().should("include", "/querying"); // সঠিক URL কিনা চেক করুন
     cy.get("#query-btn").should("contain", "Button");
-    cy.get("#query-btn").should("have.text","\n                Button\n              ");
-     
+    cy.get("#query-btn").should(
+      "have.text",
+      "\n                Button\n              "
+    );
+
     cy.get("#query-btn")
       .invoke("text")
       .then((text) => {
         expect(text.trim()).to.eq("Button");
       });
-
-//     //   cy.get("#query-btn").should("be.visible");
+cy.wait(2000);
+    cy.get(".query-list")
+      .contains("more apples")
+      .scrollIntoView() // Scroll to the element
+      .should("have.class", "fourth"); // Verify it has the expected class
+    cy.xpath("//span[normalize-space()='Save Form']").click();
+    //     //   cy.get("#query-btn").should("be.visible");
   });
 });
 
@@ -33,5 +39,44 @@ describe("My 2nd Test", () => {
 
     //  Verify that the value has been updated
     cy.get(".action-email").should("have.value", "fake@email.com");
+
+    cy.xpath("//input[@id='password1']").type("my name is sabbir");
+    // cy.scrollTo("#fullName1");
+    cy.scrollTo(0,1000);
+    cy.get("#fullName1").type("sabbir");
+    cy.get("#description").scrollIntoView().type("this is cypress learning page . best test plan.").clear();
+    
+    cy.get("#couponCode1").type("code");
+
+    // cy.xpath("//button[normalize-space()='Submit']").click();
+    cy.wait(3000);
+    cy.xpath("//button[normalize-space()='Submit']").next().should('contain', 'Your form has been submitted!');
   });
+});
+
+
+describe("3rd Test", () =>{
+
+
+    it("click diffrent places", () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("type").click();
+    cy.url().should("include", "/commands/actions");
+
+
+
+    cy.get("#action-canvas").scrollIntoView().click();
+
+    cy.get("#action-canvas").click("topLeft");
+    cy.get("#action-canvas").click("top");
+    cy.get("#action-canvas").click("topRight");
+    cy.get("#action-canvas").click("left");
+    cy.get("#action-canvas").click("right");
+    cy.get("#action-canvas").click("bottomLeft");
+    cy.get("#action-canvas").click("bottom");
+    cy.get("#action-canvas").click("bottomRight");
+  })
+
+
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,12 +3,12 @@
 describe("Google Search Test", () => {
   it('Should search for "Cypress" on Google', () => {
     // Step 1: Go to google.com
-    cy.visit("https://www.daraz.com.bd/#?");
+    cy.visit("https://demoqa.com/");
 
     // Wait for 3 seconds (3000 milliseconds)
     cy.wait(3000);
     // Step 2: Click on the search input box (Google's search box)
-    cy.xpath("//span[text()='SHOP ALL PRODUCTS']").click();
+    cy.get(".card-body").click();
 
     // cy.xpath('(//input[@name="btnK"])[1]').click({ force: true });
   });
